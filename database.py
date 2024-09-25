3f32dfda4fae4da0ac8ef65aff24d69548d7b246
Ghazal Bakhshande <bakhshande.ghazal@gmail.com>
2024-09-25 11:33:42 +0000

    change params fopr deploy

--- database.py
+++ database.py
@@ -9,10 +9,14 @@ load_dotenv()
 
 class DatabaseManager:
     def __init__(self):
-        self.server = os.getenv("DB_SERVER")
-        self.database = os.getenv("DB_NAME")
-        self.username = os.getenv("DB_USERNAME")
-        self.password = os.getenv("DB_PASSWORD")
+        # self.server = os.getenv("DB_SERVER")
+        # self.database = os.getenv("DB_NAME")
+        # self.username = os.getenv("DB_USERNAME")
+        # self.password = os.getenv("DB_PASSWORD")
+        self.server = st.secrets.get("DB_SERVER")
+        self.database = st.secrets.get("DB_NAME")
+        self.username = st.secrets.get("DB_USERNAME")
+        self.password = st.secrets.get("DB_PASSWORD")
         self.conn = None
         self.cursor = None
 
